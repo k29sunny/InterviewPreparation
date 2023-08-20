82f14f34a9894062971a55898ed8630349af3107
k29sunny <k29sunny@gmail.com>
2023-08-20 15:14:04 +0530

    Removed the unused packages and comments from the file

--- src/com/prep/java8/Student.java
+++ src/com/prep/java8/Student.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
 
 public class Student {
 	
